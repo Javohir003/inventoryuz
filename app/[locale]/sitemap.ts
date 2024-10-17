4b5c284f6d7f0b7c9e7cc06b20cfe0afbf94d2fd
Odilhon13 <odilhonsayfullayev@gmail.com>
2024-10-17 13:16:14 +0500

    check repair push

---
+++ app/[locale]/sitemap.ts
@@ -0,0 +1,42 @@
+import { Metadata, MetadataRoute } from 'next';
+import uzPages from '@/message/uz.json';
+import ruPages from '@/message/ru.json';
+import enPages from '@/message/en.json';
+
+interface Page {
+  path: string,
+  priority: number,
+  lastModified: string
+}
+
+
+export async function generateSitemap(locale: string): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'http://localhost:3000';
+
+  // Tillar va sahifalar
+  const languages: Record<string, Page[]> = {
+    uz: uzPages.pagesSitemap as Page[],
+    ru: ruPages.pagesSitemap as Page[],
+    en: enPages.pagesSitemap as Page[]
+  };
+
+  const pages = languages[locale]
+  const sitemap: MetadataRoute.Sitemap = [];
+
+  // Har bir til uchun URL-larni yaratish
+  pages.forEach((page) => {
+    sitemap.push({
+      url: `${baseUrl}/${locale}${page.path}`, // URL yaratish
+      lastModified: page.lastModified || new Date().toISOString(), // Sahifaning oxirgi o'zgarishi
+      priority: page.priority, // Sahifa priority qiymati
+    });
+  });
+
+  return sitemap;
+}
+
+export default async function sitemap(context: any): Promise<MetadataRoute.Sitemap> {
+  const locale = context.locale || 'en'; // Default locale 'en' sifatida berilgan.
+  console.log('Locale in use: ', locale); // Bu yerda locale qiymatini tekshirish
+  return generateSitemap(locale);
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 import createNextIntlPlugin from 'next-intl/plugin'
 
-
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/sitemap.xml', // sitemap.xml manzilini API-ga yo'naltirish
+        destination: '/api/sitemap.xml',
+      },
+    ];
+  },
+};
 
 export default withNextIntl(nextConfig);
