285c6fe69716c9f2cc7e91b25375e515362046c8
Javohir <yusupovjavoxir11@gmail.com>
2024-11-04 11:42:36 +0500

    new branch 404 page updated

--- app/[locale]/ClientLayout.tsx
+++ app/[locale]/ClientLayout.tsx
@@ -7,7 +7,8 @@ import { usePathname } from "next/navigation";
 export default function ClientLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname(); // Joriy URL manzilini olish
   // URL manzilini tekshirish
-  const isNotFoundPage = pathname === '/not-found' || pathname.match(/\/[a-z]{2}\/[a-z-]+/) || pathname.includes('[...not_found]');
+  const isNotFoundPage = pathname === 'not-found' || pathname.includes('[...not_found]');
+
   return (
     <>
       {/* Faqat not found sahifasi emas bo'lsa, Navbar va Footer ko'rsatilsin */}
