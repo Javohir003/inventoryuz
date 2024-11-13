412d2d54a830489a7adb8085f8e4e83ae04f3282
Javohir <yusupovjavoxir11@gmail.com>
2024-11-13 11:42:59 +0500

    video-overlay add

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,10 +1,39 @@
+"use client"
 
 import TeamListBox from "@/components/wsteam/teams";
 import "@/styles/About.css";
 import { useTranslations } from "next-intl";
+import { useState, useEffect } from "react";
 
 export default function About() {
   const t = useTranslations("About");
+  const [isVideoActive, setVideoActive] = useState(false)
+
+
+  const handleVideoActive = () => {
+    setVideoActive(!isVideoActive)
+  }
+
+  const handleCloseOverlay = (event: React.MouseEvent<HTMLDivElement>) => {
+    const target = event.target as HTMLElement;
+    if (target.classList.contains("video-overlay")) {
+      setVideoActive(false);
+    }
+  };
+  
+
+  useEffect(() => {
+    if (isVideoActive) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+
+    // Cleanup: komponent unmount bo'lganda skrollni qayta yoqish
+    return () => {
+      document.body.classList.remove("no-scroll");
+    };
+  }, [isVideoActive]);
 
   return (
     <>
@@ -61,11 +90,26 @@ export default function About() {
                   </li>
                 </ul>
               </article>
+              <div>
+               <div className="video-mask">
+                <video src="/inventoryvideo.MP4"></video>
+                  <svg onClick={handleVideoActive} className="video-anim"  viewBox="0 0 384 512"><path fill="#ffffff" d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80L0 432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
+               </div>
+              </div>
+              
+
             </div>
           </div>
           <div className="video-wrapper mb-6">
             <TeamListBox />
           </div>
+          {isVideoActive && (
+            <div className="video-overlay-w video-overlay" onClick={handleCloseOverlay}>
+              <video controls width="400px" height="300px" onClick={(e) => e.stopPropagation()}>
+                <source src="/inventoryvideo.MP4" type="video/mp4"/>
+              </video>
+            </div>
+          )}
         </div>
         <div className="Sklad-Aloqa">
           <article className="container mx-auto title-aloqa">
